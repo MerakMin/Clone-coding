8e240e29d5d6b7fe0515dc167f37d9eb8931baa1
MerakMin <ssmin413@gmail.com>
2022-09-22 20:19:35 +0900

    clonecoding commit

--- clonecoding/src/components/TodoBoard.js
+++ clonecoding/src/components/TodoBoard.js
@@ -1,10 +1,12 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
-function TodoBorad(){
+function TodoBorad(props){
 
     return(
         <div>
             <h1>Todo List</h1>
+            {props.todoList.map((item)=><TodoItem item = {item}/>)}
         </div>
     )
 }
