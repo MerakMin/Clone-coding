30c9872219ce79f3dddf37d359bd13b1d4d87e85
MerakMin <ssmin413@gmail.com>
2022-09-22 19:59:15 +0900

    clonecoding commit

--- clonecoding/src/App.js
+++ clonecoding/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Todoborad from "./components/TodoBoard"
 
 function App(){
+    const [inputValue, settInputValue] = useState('')
+    const [todoList, setTodoList] = useState([])
+        const addItem = () =>{
+            console.log("im herererere!", inputValue)
+            setTodoList([...todoList, inputValue])
+        }
     return(
         <main>
-            
+            <input value = {inputValue} type = "text" onChange={(event)=>settInputValue(event.target.value)}/>
+            <button on onClick={addItem}>추가</button>
+
+            <Todoborad/>
         </main>
     );
 }
---
+++ clonecoding/src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem() {
+
+    return(
+        <div>
+            공부하기
+        </div>
+    )
+}
+
+export default TodoItem
