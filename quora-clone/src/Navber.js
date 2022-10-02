5a37a13044e4fdb910bf09bdd32d22f6a046be48
MerakMin <ssmin413@gmail.com>
2022-10-02 18:30:39 +0900

    quora-clone commit

--- quora-clone/src/Navber.js
+++ quora-clone/src/Navber.js
@@ -1,10 +1,30 @@
 import React from "react";
 import './Navber.css'
+import {Home} from "@meterial-ui/icons"
 
 function Navber(props){
     return(
         <div className="navber">
-
+            <div className="qHeader_Logo">
+                <img src="C:\Users\ssmin\Desktop\react\quora-clone\img\Quora_logo_2015.svg.png" alt=""/>
+            </div>
+            <div className="qHeader_icons">
+                <div className="qHeader_icon">
+                    <Home/>
+                </div>
+                <div className="qHeader_icon">
+                    
+                </div>
+                <div className="qHeader_icon">
+                    
+                </div>
+                <div className="qHeader_icon">
+                    
+                </div>
+                <div className="qHeader_icon">
+                    
+                </div>
+            </div>
         </div>
     );
 }
