f8f2ce9f4ad74a0a2d40deffe10c27c362b7e6a4
MerakMin <ssmin413@gmail.com>
2022-10-02 18:09:24 +0900

    quora-clone commit

---
+++ quora-clone/src/Navber.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Navber.css'
+
+function Navber(props){
+    return(
+        <div className="navber">
+
+        </div>
+    );
+}
+
+export default Navber;
---
+++ quora-clone/src/quora.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './quora.css'
+
+function Quora() {
+    return(
+        <div className="quora">
+            <h1>반갑습니다.</h1>
+            <navber/>
+        </div>
+    );
+}
+
+export default Quora;
